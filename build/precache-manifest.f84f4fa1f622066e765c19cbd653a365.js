d06d172152d3f815ec2f8409d52cd1f64e5714e7
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-10 04:26:52 -0500

    Configuracion basica de service-worker

---
+++ build/precache-manifest.f84f4fa1f622066e765c19cbd653a365.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b3af40a5d273459bc7e",
+    "url": "/static/js/main.8b3af40a.js"
+  },
+  {
+    "revision": "8b3af40a5d273459bc7e",
+    "url": "/static/css/main.91d8507b.css"
+  },
+  {
+    "revision": "b5de0aaffa06924b15671159a51d3c24",
+    "url": "/index.html"
+  }
+];
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,3 @@
-importScripts("/precache-manifest.f84f4fa1f622066e765c19cbd653a365.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
