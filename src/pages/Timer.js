84246428406a27a50755fdba62afd675f5c9481f
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-11 19:54:34 -0500

    Permiso para enviar notificaciones

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -1,23 +1,29 @@
 import React from 'react'
 
 export default class Timer extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = { timer: 3, timeLeft: 0 }
   }
 
   start = async () => {
-    // TODO: Chequear permisos
+    // Chequear permisos
+    if (!('Notification' in window) || !('serviceWorker' in navigator))
+      return alert('Tu browser no soporta notificaciones')
+    if (Notification.permission === 'default')
+      await Notification.requestPermission()
+    if (Notification.permission === 'denied')
+      return alert('Bloqueaste las notificaciones :(')
+    if (Notification.permission === 'granted') return
 
     var timer = this.state.timer
     this.setState({ timeLeft: timer })
 
     var countdownInterval = setInterval(() => {
-      timer = timer - 1;
-      this.setState({ timeLeft: timer }) 
-      if( timer <= 0 ) { 
-        clearInterval(countdownInterval) 
+      timer = timer - 1
+      this.setState({ timeLeft: timer })
+      if (timer <= 0) {
+        clearInterval(countdownInterval)
         this.showNotification()
       }
     }, 1000)
@@ -28,22 +34,31 @@ export default class Timer extends React.Component {
   }
 
   handleChange = (e) => {
-    this.setState({timer: e.target.value})
+    this.setState({ timer: e.target.value })
   }
 
-  render () {
+  render() {
     const { timer, timeLeft } = this.state
 
-    return <div className="Timer">
-      <div className="name">Timer</div>
-      { timeLeft === 0 ? 
-        <div className="center">
-          <input type="number" min="0" max="999" step="1" value={timer} onChange={this.handleChange} />
-          <button onClick={ this.start }>Start</button>
-        </div>
-      :
-        <div className="timeLeft">{ timeLeft }s</div>
-      }
-    </div>
+    return (
+      <div className="Timer">
+        <div className="name">Timer</div>
+        {timeLeft === 0 ? (
+          <div className="center">
+            <input
+              type="number"
+              min="0"
+              max="999"
+              step="1"
+              value={timer}
+              onChange={this.handleChange}
+            />
+            <button onClick={this.start}>Start</button>
+          </div>
+        ) : (
+          <div className="timeLeft">{timeLeft}s</div>
+        )}
+      </div>
+    )
   }
 }
